14862b3efee5720f9f0cc954f04665597678ed19
ashvegeta <ashik2001mp@gmail.com>
2024-08-08 05:39:22 +0000

    added funcs for handling Google storage and firestore

--- go.mod
+++ go.mod
@@ -4,17 +4,19 @@ go 1.22.5
 
 require cloud.google.com/go/storage v1.43.0
 
+require cloud.google.com/go/longrunning v0.5.11 // indirect
+
 require (
 	cloud.google.com/go v0.115.0 // indirect
 	cloud.google.com/go/auth v0.7.3 // indirect
 	cloud.google.com/go/auth/oauth2adapt v0.2.3 // indirect
 	cloud.google.com/go/compute/metadata v0.5.0 // indirect
+	cloud.google.com/go/firestore v1.16.0
 	cloud.google.com/go/iam v1.1.12 // indirect
 	github.com/felixge/httpsnoop v1.0.4 // indirect
 	github.com/go-logr/logr v1.4.2 // indirect
 	github.com/go-logr/stdr v1.2.2 // indirect
 	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
-	github.com/golang/protobuf v1.5.4 // indirect
 	github.com/google/s2a-go v0.1.8 // indirect
 	github.com/google/uuid v1.6.0 // indirect
 	github.com/googleapis/enterprise-certificate-proxy v0.3.2 // indirect
@@ -32,7 +34,7 @@ require (
 	golang.org/x/sys v0.22.0 // indirect
 	golang.org/x/text v0.16.0 // indirect
 	golang.org/x/time v0.6.0 // indirect
-	google.golang.org/api v0.191.0 // indirect
+	google.golang.org/api v0.191.0
 	google.golang.org/genproto v0.0.0-20240730163845-b1a4ccb954bf // indirect
 	google.golang.org/genproto/googleapis/api v0.0.0-20240725223205-93522f1f2a9f // indirect
 	google.golang.org/genproto/googleapis/rpc v0.0.0-20240730163845-b1a4ccb954bf // indirect
--- src/DB/main.go
+++ src/DB/main.go
@@ -5,27 +5,45 @@ import (
 	"fmt"
 	"log"
 
+	"cloud.google.com/go/firestore"
 	"cloud.google.com/go/storage"
 	"google.golang.org/api/iterator"
-	// "cloud.google.com/go/firestore"
 )
 
-func main() {
-	ctx := context.Background()
-
+func GoogleStorage(ctx context.Context) (*storage.Client, *storage.BucketHandle) {
 	// Replace with path to your service account key file
 	client, err := storage.NewClient(ctx)
 	if err != nil {
 		log.Fatalf("Failed to create client: %v", err)
 	}
-	defer client.Close()
 
 	// // declare bucket info
 	bucketName := "socialmediaapp-431315.appspot.com"
 	bucket := client.Bucket(bucketName)
 
-	// // List objects in the bucket
-	it := bucket.Objects(ctx, nil)
+	return client, bucket
+}
+
+func Firestore(ctx context.Context) *firestore.Client {
+	// Sets your Google Cloud Platform project ID.
+	projectID := "socialmediaapp-431315"
+
+	client, err := firestore.NewClient(ctx, projectID)
+	if err != nil {
+		log.Fatalf("Failed to create client: %v", err)
+	}
+
+	return client
+}
+
+func main() {
+	// Google Storage
+	ctx1 := context.Background()
+	gsClient, bucket := GoogleStorage(ctx1)
+	defer gsClient.Close()
+
+	// List objects in the bucket
+	it := bucket.Objects(ctx1, nil)
 
 	for {
 		attrs, err := it.Next()
@@ -37,5 +55,21 @@ func main() {
 		}
 
 		fmt.Printf("Object: %s\n", attrs.Name)
+	}
+
+	// Firestore
+	ctx2 := context.Background()
+
+	fsClient := Firestore(ctx2)
+	defer fsClient.Close()
+
+	_, _ , err := fsClient.Collection("users").Add(ctx2, map[string]interface{}{
+		"first": "Ada",
+        "last":  "Lovelace",
+        "born":  1815,
+	})
+
+	if err != nil {
+		log.Fatalf("failed to add entry : %v", err)
 	}
 }
