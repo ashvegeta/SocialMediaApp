2cf085a56dfdee9e499364756b36aef280c3a927
ashvegeta <ashik2001mp@gmail.com>
2024-08-09 03:27:31 +0000

    user handlers boilerplate

---
+++ src/DB/users/userCRUD.go
@@ -0,0 +1,33 @@
+package users
+
+func AddUser() {
+
+}
+
+func DelUser() {
+
+}
+
+func UpdateUserInfo() {
+
+}
+
+func AddPost() {
+
+}
+
+func DelPost() {
+
+}
+
+func UpdatePost() {
+
+}
+
+func AddConnection() {
+
+}
+
+func DelConnection() {
+
+}
